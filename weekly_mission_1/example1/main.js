1657fdc615dae94fd7cc686d44da01ce05b3616b
Axel Reyes <rbaxel.f@gmail.com>
2022-04-15 19:29:42 -0500

    add:creación de un objeto y sus valores

---
+++ weekly_mission_1/example1/main.js
@@ -0,0 +1,6 @@
+// 1. Creación de un objeto con propiedades
+
+let myCar = new  Object(); //Creación de un objeto
+myCar.make = 'Ford'; //Guardando un valor dentro del objeto creado
+myCar.model = 'Mustang';
+myCar.year = '1969':
