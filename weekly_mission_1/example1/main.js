9e23294dd5c0efa22b63f0663c9f595bd61b5629
Axel Reyes <rbaxel.f@gmail.com>
2022-04-15 19:33:39 -0500

    fix:mainJs

--- weekly_mission_1/example1/main.js
+++ weekly_mission_1/example1/main.js
@@ -3,4 +3,6 @@
 let myCar = new  Object(); //Creación de un objeto
 myCar.make = 'Ford'; //Guardando un valor dentro del objeto creado
 myCar.model = 'Mustang';
-myCar.year = '1969':
+myCar.year = '1969';
+
+console.log(myCar);
