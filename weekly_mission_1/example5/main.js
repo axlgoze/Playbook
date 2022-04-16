ad174b514f93044275be05bf76f66799176bcaac
Axel Reyes <rbaxel.f@gmail.com>
2022-04-15 22:33:01 -0500

    add:comment on main.js

--- weekly_mission_1/example5/main.js
+++ weekly_mission_1/example5/main.js
@@ -2,3 +2,9 @@ const logger =require('./logger') //requiere del módulo -parte de código- de l
 
 //Ya se puede usar directamente el objeto instanciado en el módulo logger
 logger.log('This is an informational message')
+
+/*
+ También puedes instanciar uno nuevo de esta manera:
+	const customerLogger = new logger.constructor('CUSTOM')
+	customLogger.log('This is an informational message')
+*/
