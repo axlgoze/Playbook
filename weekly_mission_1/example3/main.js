fd48182d22c02cbaa1cf61dedc31b72c9903438c
Axel Reyes <rbaxel.f@gmail.com>
2022-04-15 21:35:42 -0500

    fix:main.js

--- weekly_mission_1/example3/main.js
+++ weekly_mission_1/example3/main.js
@@ -6,6 +6,6 @@ const logger1 = require('./logger1')
 const logger2 = require('./logger2')
 
 logger1("This is an informational message")
-logger2("This is a verbose informational message")
+logger2.verbose("This is a verbose informational message")
 
 
