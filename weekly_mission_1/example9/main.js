39a234b3fe95b3e67d9ab99356a6210d229366b2
Axel Reyes <rbaxel.f@gmail.com>
2022-04-17 15:31:58 -0500

    add: main y dependencia de modulo creador de objetos

---
+++ weekly_mission_1/example9/main.js
@@ -0,0 +1,11 @@
+const Pokemon = require('./pokemon') //dependencia con módulo pokemon
+
+const pikachu = new Pokemon("pikachu") //Creación de objetos
+
+const bulbasaur = new Pokemon("bulbasaur")
+
+const squirtle = new Pokemon("squirtle")
+
+const charmander = new Pokemon("charmander")
+
+
