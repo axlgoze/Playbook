5a15eb79a6cc0978fe880d09ebe52a8b5ef35cb1
Axel Reyes <rbaxel.f@gmail.com>
2022-04-17 01:42:49 -0500

    add: llamadas al modulo modificador de objeto y el modulo del objeto

--- weekly_mission_1/example6/main.js
+++ weekly_mission_1/example6/main.js
@@ -1,1 +1,4 @@
 require('./patcher') //Llamas al módulo que modifica el objeto instanciado
+const logger = require('./logger') //llamas al módulo logger.js y te dará el obejto modificado
+
+logger.customMessage()
