9124f7c11a8a2bf44c9ac8235376ff2617ad82f5
Axel Reyes <rbaxel.f@gmail.com>
2022-04-17 01:34:25 -0500

    AGregando modulo y exportando objeto y clase

--- weekly_mission_1/example6/logger.js
+++ weekly_mission_1/example6/logger.js
@@ -1,3 +1,17 @@
 //Modificando clases
+class Logger{
+	constructor(name){
+		this.count= 0
+		this.name = name
+	}
+
+	log(message){
+		this.count++
+		console.log('['+ this.name + ']' + message)
+	}
+}
+
+module.exports = new Logger('DEFAULT') //Nuevo objeto instanciado
+module.exports.Logger = new Logger //clase 
 
 
