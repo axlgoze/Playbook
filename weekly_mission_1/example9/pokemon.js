4aefa985acd794bb52ace12924d5fbd8dfd1c9a4
Axel Reyes <rbaxel.f@gmail.com>
2022-04-18 23:58:35 -0500

    agregando metodo sayHello

--- weekly_mission_1/example9/pokemon.js
+++ weekly_mission_1/example9/pokemon.js
@@ -3,7 +3,15 @@
 class Pokemon{
 	//constructor
 	constructor(name){
-		this.name=name
+		//this.name es la variable que se guarda en el contexto local
+		this.name=name //atributo nombre del pokemon
 	}
+
+	//método di hola
+	//message es una variable que se le pasa al ejecutar en el contexto local
+	sayHello (message){
+		console.log(`Mi pokemon ${this.name} te saluda!!!`)
+	}
+
 }
 
