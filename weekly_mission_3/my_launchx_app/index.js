60de75680b38f9d7edb53c88089e0287192c0758
Axel Reyes <rbaxel.f@gmail.com>
2022-04-21 17:08:19 -0500

    imprimiendo atributos

--- weekly_mission_3/my_launchx_app/index.js
+++ weekly_mission_3/my_launchx_app/index.js
@@ -5,3 +5,8 @@ const MissionCommander = require('.app/missionCommander');
 const carlo = new MissionCommander("Carlo");
 const axel = new MissionCommander("Axel");
 const jhonny = new MissionCommander("Jhonny");
+
+//imprimiendo objetos
+console.log(carlo.name) //imprime el atributo name
+console.log(axel.name)
+console.log(jhonny.name)
