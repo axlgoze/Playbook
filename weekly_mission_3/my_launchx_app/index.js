d9e6fd03105033700dd756329cb2192bf956e005
Axel Reyes <rbaxel.f@gmail.com>
2022-04-21 17:06:49 -0500

    instanciando tres objetos

--- weekly_mission_3/my_launchx_app/index.js
+++ weekly_mission_3/my_launchx_app/index.js
@@ -1,3 +1,7 @@
 //importando clase
 const MissionCommander = require('.app/missionCommander');
 
+//instanciando  objetos
+const carlo = new MissionCommander("Carlo");
+const axel = new MissionCommander("Axel");
+const jhonny = new MissionCommander("Jhonny");
