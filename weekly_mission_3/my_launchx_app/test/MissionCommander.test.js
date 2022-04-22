deb82032f3a283c780589df6919b623059dcffd1
Axel Reyes <rbaxel.f@gmail.com>
2022-04-21 23:19:12 -0500

    testeando un error

--- weekly_mission_3/my_launchx_app/test/MissionCommander.test.js
+++ weekly_mission_3/my_launchx_app/test/MissionCommander.test.js
@@ -3,7 +3,7 @@ const MissionCommander = require('./../app/missionCommander');
 describe("Unit Tests for Mission Commander Class", () => {
 	test('1) Create a mission commander object', () => {
 		// Aqui puedes usar el código como lo deseas utilizar
-		const myMissionCommander = new MissionCommander ("Woopa")
+		const myMissionCommander = new MissionCommander ("Woopas")
 		// validar el resultado esperado
 		expect(myMissionCommander.name).toBe("Woopa"); //el primer parametro es la propiedad a probarm, el siguiente parentesis es el valor que esperas obtener
 
