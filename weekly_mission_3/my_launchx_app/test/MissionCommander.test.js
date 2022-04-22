0650c495a6b734b429d3447f73b09beef09a98ca
Axel Reyes <rbaxel.f@gmail.com>
2022-04-21 23:16:55 -0500

    fix:name of module imported

--- weekly_mission_3/my_launchx_app/test/MissionCommander.test.js
+++ weekly_mission_3/my_launchx_app/test/MissionCommander.test.js
@@ -1,4 +1,4 @@
-const MissionCommander = require('./../app/misionCommander');
+const MissionCommander = require('./../app/missionCommander');
 
 describe("Unit Tests for Mission Commander Class", () => {
 	test('1) Create a mission commander object', () => {
