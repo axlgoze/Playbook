3cf6bd83a2562c55a8848f7d4be402027627c12a
Axel Reyes <rbaxel.f@gmail.com>
2022-04-21 23:07:43 -0500

    instanciando objeto

--- weekly_mission_3/my_launchx_app/test/MissionCommander.test.js
+++ weekly_mission_3/my_launchx_app/test/MissionCommander.test.js
@@ -3,7 +3,7 @@ const MissionCommander = require('./../app/misionCommander');
 describe("Unit Tests for Mission Commander Class", () => {
 	test('1) Create a mission commander object', () => {
 		// Aqui puedes usar el código como lo deseas utilizar
-		const result = 1 + 7
+		const myMissionCommander = new MissionCommander ("Woopa")
 		// validar el resultado esperado
 		expect(result).toBe(10);
 	})
