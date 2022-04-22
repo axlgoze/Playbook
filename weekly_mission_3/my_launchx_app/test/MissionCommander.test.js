630ba2c0626f2e046d52ed49c79e8f908289558c
Axel Reyes <rbaxel.f@gmail.com>
2022-04-21 22:59:01 -0500

    haciendo pruebas

--- weekly_mission_3/my_launchx_app/test/MissionCommander.test.js
+++ weekly_mission_3/my_launchx_app/test/MissionCommander.test.js
@@ -1,7 +1,7 @@
 describe("Esto es una suite de pruebas", () => {
 	test('Caso de pŕueba 1', () => {
 		// Aqui puedes usar el código como lo deseas utilizar
-		const result = 1 + 9
+		const result = 1 + 7
 		// validar el resultado esperado
 		expect(result).toBe(10);
 	})
